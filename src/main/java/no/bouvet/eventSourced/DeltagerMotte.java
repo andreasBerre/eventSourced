117683b4d8cbf36bdf38f0a12da0c9dd27e64fa3
Andreas Berre <andreas.berre@bouvet.no>
2013-04-26 09:47:53 +0200

    fields for parametere på eventen

--- src/main/java/no/bouvet/eventSourced/DeltagerMotte.java
+++ src/main/java/no/bouvet/eventSourced/DeltagerMotte.java
@@ -1,6 +1,19 @@
 package no.bouvet.eventSourced;
 
 public class DeltagerMotte {
+    private final String presentasjonTittel;
+    private final String deltagerNavn;
+
     public DeltagerMotte(String presentasjonTittel, String deltagerNavn) {
+        this.presentasjonTittel = presentasjonTittel;
+        this.deltagerNavn = deltagerNavn;
+    }
+
+    public String getPresentasjonTittel() {
+        return presentasjonTittel;
+    }
+
+    public String getDeltagerNavn() {
+        return deltagerNavn;
     }
 }
