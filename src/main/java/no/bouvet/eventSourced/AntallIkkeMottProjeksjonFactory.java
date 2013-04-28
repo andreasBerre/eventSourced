bc1dd9a27d64b00605a59f0532c852ca729d8f1d
Andreas Berre <andreas.berre@gmail.com>
2013-04-28 10:48:03 +0200

    inn med en todo for å beskrive nødvendige endringer

--- src/main/java/no/bouvet/eventSourced/AntallIkkeMottProjeksjonFactory.java
+++ src/main/java/no/bouvet/eventSourced/AntallIkkeMottProjeksjonFactory.java
@@ -5,6 +5,7 @@ import akka.actor.ActorRef;
 import akka.actor.UntypedActorFactory;
 import akka.testkit.TestActorRef;
 
+//TODO: Gjøre ferdig, denne klassen skal lage antallIkkeMøtt projeksjonen
 public class AntallIkkeMottProjeksjonFactory implements UntypedActorFactory {
     public AntallIkkeMottProjeksjonFactory(ActorRef eventStoreRef) {
     }
